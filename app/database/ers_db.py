3e15e86ee4e9b19ff231c412e15ad5bc8304ff44
parkjbdev <dev@parkjb.com>
2025-04-05 17:17:35 +0900

    feat: log database connection

--- app/database/ers_db.py
+++ app/database/ers_db.py
@@ -1,7 +1,9 @@
-from typing import Optional
-import asyncpg
+import logging
 import os
+from logging import Logger
+from typing import Optional
 
+import asyncpg
 from asyncpg import Pool
 
 from app.database.interface import Database
@@ -12,21 +14,38 @@ from app.utils.decorators import singleton
 class ExamReservationSystemDatabase(Database):
     def __init__(self):
         self.__pool: Optional[Pool] = None
+        self.__logger: Logger = logging.getLogger(__name__)
 
     async def connect(self):
-        print("connecting")
         if self.__pool is None:
-            self.__pool = await asyncpg.create_pool(
-                os.getenv("DATABASE_URL"),
-                min_size=5,
-                max_size=10,
-                server_settings={'search_path': os.getenv("APP_DB_SCHEMA")}
-            )
+            self.__logger.info("Connecting to the database...")
+            try:
+                self.__pool = await asyncpg.create_pool(
+                    os.getenv("DATABASE_URL"),
+                    min_size=5,
+                    max_size=10,
+                    server_settings={'search_path': os.getenv("APP_DB_SCHEMA")}
+                )
+            except asyncpg.PostgresError as e:
+                self.__logger.exception("Failed to connect to the database.")
+                raise
+            except Exception as e:
+                self.__logger.exception(f"Unknown Exception: {e}")
+                raise
+            self.__logger.info("Connected to the database.")
 
     async def disconnect(self):
         if self.__pool:
-            await self.__pool.close()
+            self.__logger.info("Disconnecting from the database...")
+            try:
+                await self.__pool.close()
+                self.__logger.info("Disconnected from the database.")
+            except Exception as e:
+                self.__logger.exception(f"Failed to disconnect from the database: {e}")
+                raise
             self.__pool = None
+        else:
+            self.__logger.warning("No active database connection to disconnect.")
 
     def get_pool(self) -> Pool:
         if self.__pool is None:
