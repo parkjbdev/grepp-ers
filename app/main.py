df56a53eeb0926f8f4357d2b080feeb1699c0117
parkjbdev <dev@parkjb.com>
2025-04-08 03:35:54 +0900

    feat: Add exception handlers for custom exceptions

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app.controllers.admin_reservations import router as admin_controller
 from app.controllers.auth import router as auth_controller
 from app.controllers.slot import router as slot_controller
 from app.controllers.user_reservations import router as reservation_controller
+from app.services.exceptions import DBConflictException, DBUnknownException, NotFoundException, UserNotFoundException
 
 
 @asynccontextmanager
@@ -48,4 +49,32 @@ async def exception_handler(request, exc):
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         content={"detail": "Internal server error"}
+    )
+
+@app.exception_handler(DBUnknownException)
+async def db_unknown_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"detail": "Database error"}
+    )
+
+@app.exception_handler(DBConflictException)
+async def db_conflict_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
+        content={"detail": str(exc)}
+    )
+
+@app.exception_handler(NotFoundException)
+async def db_unknown_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"detail": "Database error"}
+    )
+
+@app.exception_handler(UserNotFoundException)
+async def user_not_found_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"detail": str(exc)}
     )
