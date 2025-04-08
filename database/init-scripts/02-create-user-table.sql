0c81b5d82de264d398ee2efaa25f4c1c1ec8b6a0
parkjbdev <dev@parkjb.com>
2025-04-09 01:08:19 +0900

    test: add default admin and user

--- database/init-scripts/02-create-user-table.sql
+++ database/init-scripts/02-create-user-table.sql
@@ -6,3 +6,14 @@ CREATE TABLE users
     admin      BOOLEAN                  DEFAULT FALSE             NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+-- TODO: REMOVE THIS AFTER TESTING
+-- Test Admin
+INSERT INTO users (username, password, admin)
+VALUES ('admin', '$argon2id$v=19$m=65536,t=3,p=4$zmxfD0y7SqFVHtQEIuNDXg$vJmxTwkDpEpQ4SPj2L6GRwozxnB0O9sngkkr1akNFic',
+        TRUE);
+
+-- Test User
+INSERT INTO users (username, password, admin)
+VALUES ('user', '$argon2id$v=19$m=65536,t=3,p=4$zmxfD0y7SqFVHtQEIuNDXg$vJmxTwkDpEpQ4SPj2L6GRwozxnB0O9sngkkr1akNFic',
+        TRUE);
