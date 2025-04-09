464422d5fb7e01af1ca1c5873cdc1123b693ce57
parkjbdev <dev@parkjb.com>
2025-04-09 14:20:27 +0900

    docs: Update exception messages for Korean localization

--- app/repositories/exception.py
+++ app/repositories/exception.py
@@ -2,8 +2,8 @@ class NoSuchElementException(Exception):
     def __init__(self, elem_name: str, condition: str):
         self.elem_name = elem_name
         self.condition = condition
-        self.message = f"{elem_name} with {condition} does not exist"
+        self.message = f"{condition} 조건을 가진 {elem_name}이 존재하지 않습니다."
         super().__init__(self.message)
 
     def __str__(self):
-        return f"{self.elem_name} with {self.condition} does not exist"
+        return self.message
--- app/repositories/reservation/exceptions.py
+++ app/repositories/reservation/exceptions.py
@@ -3,7 +3,7 @@ from app.repositories.exception import NoSuchElementException
 
 class SlotLimitExceededException(Exception):
     def __init__(self):
-        self.message = "The maximum number of slots for this time period has been exceeded"
+        self.message = "본 시험의 예약 가능 인원이 초과되어 실패하였습니다."
         super().__init__(self.message)
 
 
@@ -14,17 +14,18 @@ class NoSuchReservationException(NoSuchElementException):
 
 class ReservationAlreadyConfirmedException(Exception):
     def __init__(self, reservation_id: int):
-        self.message = f"Reservation {reservation_id} is already confirmed. Only unconfirmed reservations can be modified by user. Ask admin to modify."
+        self.message = f"{reservation_id}번 예약은 이미 확정되었습니다. 사용자가 수정할 수 있는 예약은 미확정 예약만 가능합니다. 관리자에게 수정 요청하세요."
         super().__init__(self.message)
 
 
 class UserMismatchException(Exception):
     def __init__(self, user_id: int):
-        self.message = f"User ID {user_id} does not match the reservation. Only unconfirmed reservations can be deleted by user. Ask admin to delete."
+        # TODO: Replace with NoSuchReservationException for security?
+        self.message = f"{user_id}번 사용자는 요청한 예약을 변경할 권한이 없습니다. 본인의 예약만 삭제할 권한이 있습니다."
         super().__init__(self.message)
 
 
 class DaysNotLeftEnoughException(Exception):
     def __init__(self, days: int):
-        self.message = f"Reservation can be made only {days} days in advance."
+        self.message = f"예약은 시험으로부터 {days}일 이전에만 가능합니다."
         super().__init__(self.message)
